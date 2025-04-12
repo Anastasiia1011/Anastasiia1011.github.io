0d4a126f5343d7f72578499b2d6f131d17d7d5e0
Anastasiia1011 <anastasiiashiman@gmail.com>
2025-04-12 17:38:48 -0600

    js

---
+++ script/script.js
@@ -0,0 +1,29 @@
+//Alert Button
+document.getElementById("alertMe").onclick = function () {alert("Hello world!")};
+
+//Hober Button
+const hoverBtn = document.getElementById("hoverButton");
+hoverBtn.onmouseover = function () {hoverBtn.style.background = "lightblue";};
+hoverBtn.onmouseleave = function () {hoverBtn.style.background = "lightgreen";};
+
+//Incrementing Button + If/Else Coloring 
+let count = 0;
+document.getElementById("buttonCounter").onclick = function () {
+    count +=1;
+    const counter = document.getElementById("counter");
+    counter.innerHTML = "Count:" + count;
+
+    if (count % 2 === 0) {
+        counter.style.color = "green";
+    } else {
+        counter.style.color = "red";
+    }
+};
+
+//For loop Messages
+const container = document.getElementById("container");
+for (let i = 0; i < 5; i++) {
+    const para = document.createElement("p");
+    para.innerText = "Rabbits are awesome!";
+    container.apppendChild(para);
+}
