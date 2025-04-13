bd5166e32fecf5cbd15f514e6b322c43ed933eec
Anastasiia1011 <anastasiiashiman@gmail.com>
2025-04-12 21:20:03 -0600

    fixing

--- script/script.js
+++ script/script.js
@@ -1,33 +1,34 @@
 console.log("Hello World");
-
+window.onload = function () {
 //Alert Button
-document.getElementById("alertMe").onclick = function () {alert("Hello world!");
+document.getElementById("alertMe").onclick= function () {alert("Hello world!");
 
 };
 
 //Hober Button
-const hoverBtn = document.getElementById("hoverButton");
+const hoverBtn= document.getElementById("hoverButton");
 hoverBtn.onmouseover = function () {hoverBtn.style.background = "lightblue";};
 hoverBtn.onmouseleave = function () {hoverBtn.style.background = "lightgreen";};
 
 //Incrementing Button + If/Else Coloring 
-let count = 0;
-document.getElementById("buttonCounter").onclick = function () {
+let count= 0;
+document.getElementById("buttonCounter").onclick= function () {
     count +=1;
-    const counter = document.getElementById("counter");
-    counter.innerHTML = "Count:" + count;
+    const counter= document.getElementById("counter");
+    counter.innerHTML= "Count:" + count;
 
     if (count % 2 === 0) {
-        counter.style.color = "green";
+        counter.style.color= "green";
     } else {
-        counter.style.color = "red";
+        counter.style.color= "red";
     }
 };
 
 //For loop Messages
-const container = document.getElementById("container");
+const container= document.getElementById("container");
 for (let i = 0; i < 5; i++) {
     const para = document.createElement("p");
     para.innerText = "Rabbits are awesome!";
     container.appendChild(para);
 }
+};
